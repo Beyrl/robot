5ee997f82fc0411f517b66588c02d898cd77199d
AmandaPan <amanda@AmandaPandeMBP.lan>
2020-09-13 14:35:26 +0800

    add portrait

--- html5-1/js/bot.js
+++ html5-1/js/bot.js
@@ -4,16 +4,16 @@ $(function () {
         inputVal = $('#btnBox').parent().find('input').val(),
         userChatHtml="",
         BotChatHtml="";
-        userChatHtml +=`<div class="user">
+        userChatHtml =`<div class="user">
                             <div class="content">${inputVal}</div>
                             <div class="portrait">Use</div>
                         </div>`;
-        
+        if(!inputVal){return }
         $("#chatContent").append(userChatHtml);
         $('#btnBox').parent().find('input').val("");
         $("#chatContent").scrollTop($("#chatContent")[0].scrollHeight);
         setTimeout(()=>{
-            BotChatHtml +=`<div class="robot">
+            BotChatHtml =`<div class="robot">
                                 <div class="portrait"></div>
                                 <div class="content">I can't understand, can you try some other questions.</div>
                             </div>`;
